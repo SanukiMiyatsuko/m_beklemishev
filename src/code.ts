5e92cc6b6d5d5e9ce4380536485b2bca7c285021
taketori <naoki123453@gmail.com>
2025-02-09 01:39:32 +0900

    型にnullを追加し管理をしやすく 余計に多かったfpを減らす

--- src/code.ts
+++ src/code.ts
@@ -6,24 +6,21 @@ export function expand(seq: number[], n: number, M: number): number[] {
         const setP = (n: number): number[] => {
             const result: number[] = [];
             const pPredn = p(n-1);
-            if (pPredn === -1) return result;
-            let i = pPredn-1;
-            while (i > -1) {
-                if (lt(seq.slice(i), seq.slice(pPredn))) result.push(i);
-                i -= 1;
-            }
+            if (pPredn === null) return result;
+            for (let i = pPredn-1; i > -1; i--)
+                if (lt(seq.slice(i), seq.slice(pPredn)))
+                    result.push(i);
             return result.reverse();
         }
-        const p = (n: number): number => {
+        const p = (n: number): number | null => {
             if (n === 0) return p0;
-            if (setP(n).length === 0) return -1;
+            if (setP(n).length === 0) return null;
             return Math.max(...setP(n));
         }
         const u = (x: number, n: number): number => {
             let i: number;
-            for (i of setP(n)) {
+            for (i of setP(n))
                 if (x < i) return i;
-            }
             return p0;
         }
         const fp = (x: number, n: number): number => {
@@ -33,26 +30,26 @@ export function expand(seq: number[], n: number, M: number): number[] {
         const plist = [p0];
         for (let m = 1; m < M; m++) {
             const pm = p(m);
-            if (pm === -1 || 2 <= last-seq[pm]) return pm;
-            plist.push(pm);
+            if (pm === null) return -1;
+            if (2 <= last-seq[pm]) return pm;
             for (let n = 1; n < m; n++)
                 if (lt(seq.slice(pm,u(pm,n)),seq.slice(plist[n],plist[n-1])))
                     return fp(pm,n);
+            plist.push(pm);
         }
         const pM = p(M);
-        if (pM === -1 || 2 <= last-seq[pM]) return pM;
-        plist.push(pM);
-        for (let n = 1; n < M; n++)
+        if (pM === null) return -1;
+        if (2 <= last-seq[pM]) return pM;
+        for (let n = 1; n < M-1; n++)
             if (lt(seq.slice(pM,u(pM,n)),seq.slice(plist[n],plist[n-1])))
                 return fp(pM,n);
-        return fp(pM,M);
+        return fp(pM,M-1);
     })()
     const GP = seq.slice(0,parent+1);
     const bp = seq.slice(parent+1,-1).concat([last-1]);
     let BP: number[] = [];
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++)
         BP = BP.concat(bp);
-    }
     return GP.concat(BP);
 }
 
